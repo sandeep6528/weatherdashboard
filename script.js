e8986b4f40b83d1a40154bfb19c0e6709bf53014
Sandeep Meegahapola <smeegahapola5@gmail.com>
2023-10-01 20:33:20 -0500

    started adding javascript to make it function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const cityInput =  document.querySelector(".city-input");
+ const searchButton = document.querySelector(".search-btn");
 
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if(!cityName) return;
+
+    console.log(cityName);
+}
+
+searchButton.addEventListener("click", getCityCoordinates);
